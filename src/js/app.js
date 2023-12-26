ae3b59c736df50df54e5dbbb5e47fdc88441e970
Alyxia Sother <alyxia@riseup.net>
2023-12-26 15:23:48 +0100

    [js/app] Lazy load images

--- src/js/app.js
+++ src/js/app.js
@@ -4,4 +4,17 @@ Alpine.start()
 
 import '@/css/app.scss'
 
-console.log("hi")
+const main = async () => {
+    const lazysizes = await import(/* webpackChunkName: "lazysizes" */'lazysizes');
+    return null;
+};
+
+main().then(() => {
+    // Hide all the placeholder images when lazyload image is unveiled (also works with AJAX loaded content)
+    document.addEventListener('lazyloaded', function(e){
+        const imageThatJustLoaded = e.target;
+        if (imageThatJustLoaded.previousElementSibling && imageThatJustLoaded.previousElementSibling.classList.contains('placeholder')) {
+            imageThatJustLoaded.previousElementSibling.classList.add('opacity-0');
+        }
+    });
+})

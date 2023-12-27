df7c2e7bef4e015fc3fa5e419d9819257c782f24
Alyxia Sother <alyxia@riseup.net>
2023-12-27 01:00:43 +0100

    [templates] Add buttons to image sliders

---
+++ config/inlinesvg.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    // Enter the paths to the directories where you store your individual SVG
+    // files. Absolute paths should be used, which is simple using aliases
+    // such as @webroot. (https://docs.craftcms.com/v3/config/#aliases)
+
+    'paths' => [
+        '@webroot/img',
+    ],
+    'class' => '',
+
+];
